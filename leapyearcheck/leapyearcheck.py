feec95f07a905398fd7366616c373bfd1cf2a8bc
xhydrogenx <115335592+xhydrogenx@users.noreply.github.com>
2023-09-25 22:21:09 +0700

    Визуал

--- leapyearcheck/leapyearcheck.py
+++ leapyearcheck/leapyearcheck.py
@@ -2,29 +2,31 @@ import PySimpleGUI as sg
 
 layout = [
     [sg.Text('Введите год', size=(25, 1), key='-text-', font='Helvetica 16')],
-    [sg.InputText('', enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
+    [sg.InputText('', enable_events=True, key='-INPUT-', font='Helvetica 16')],
     [sg.Button('Проверить', enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
     [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
 
 window = sg.Window('Проверка года на високосный', layout, size=(350, 350))
 
-# year = int(input("Введите год: "))
-# leap = 'Високосный'
-# regular = 'Обычный'
-#
-# if year % 4 != 0:
-#     print(regular)
-# elif year % 100 == 0:
-#     if year % 400 == 0:
-#         print(leap)
-#     else:
-#         print(regular)
 
-# запускаем основной бесконечный цикл
+def year_check():
+    print(values)
+    year = int(values['-INPUT-'])
+    leap = 'Високосный'
+    regular = 'Обычный'
+
+    if year % 4 == 0 or (year % 100 != 0 and year % 400 == 0):
+        print(leap)
+    else:
+        print(regular)
+
 
 while True:
     # получаем события, произошедшие в окне
     event, values = window.read()
+    # если отдает function - запустить функцию year_check
+    if event == '-FUNCTION-':
+        year_check()
     # если нажали на крестик
     if event in (sg.WIN_CLOSED, 'Exit'):
         # выходим из цикла
