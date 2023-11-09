eeedcc13fcd5eb2e4c137d1c5a7f47c8a395e11b
hydrogen <ivan.karpov.88@yandex.ru>
2023-11-09 15:55:15 +0700

    Добавлена запись информации об удаленных файлах в .txt

--- lab9/app.py
+++ lab9/app.py
@@ -4,6 +4,7 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPu
 from module_1 import find_and_remove_duplicates
 from module_2 import sort_photos_by_date
 
+
 class PhotoOrganizerApp(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -28,9 +29,9 @@ class PhotoOrganizerApp(QMainWindow):
         self.sort_photos_button.clicked.connect(self.sort_photos_by_date)
 
     async def find_and_remove_duplicates(self):
-
         directory_to_scan = "C:/Users/hydrogen/PycharmProjects/labskarpov/test"
-        await find_and_remove_duplicates(directory_to_scan)
+        log_file_path = "duplicate_log.txt"
+        await find_and_remove_duplicates(directory_to_scan, log_file_path)
 
     def start_find_and_remove_duplicates(self):
         asyncio.run(self.find_and_remove_duplicates())
@@ -38,6 +39,7 @@ class PhotoOrganizerApp(QMainWindow):
     def sort_photos_by_date(self):
         directory_to_sort = "C:/Users/hydrogen/PycharmProjects/labskarpov/test"
         sort_photos_by_date(directory_to_sort)
+
 
 if __name__ == '__main__':
     app = QApplication([])
--- lab9/module_1.py
+++ lab9/module_1.py
@@ -3,24 +3,27 @@ import hashlib
 import asyncio
 
 
-async def find_and_remove_duplicates(directory):
+async def find_and_remove_duplicates(directory: str, log_file: str):
     """Поиск файлов происходит по хэшу"""
     file_hashes = {}
 
-    for root, _, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            with open(file_path, "rb") as f:
-                file_content = f.read()
-                f.close()
-                file_hash = hashlib.md5(file_content).hexdigest()
-                if file_hash in file_hashes:
-                    # Этот файл - дубликат
-                    print(f"Дубликат найден: {file_path}")
-                    try:
-                        os.unlink(file_path)
-                    except PermissionError as e:
-                        print(f"Ошибка при удалении файла {file_path}: {e}")
-                else:
-                    file_hashes[file_hash] = file_path
-    return print("Дубликатов не найдено")
+    with open(log_file, "a", encoding="utf-8") as log:
+        log.truncate(0)
+        for root, _, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, "rb") as f:
+                    file_content = f.read()
+                    f.close()
+                    file_hash = hashlib.md5(file_content).hexdigest()
+                    if file_hash in file_hashes:
+                        duplicate_path = file_hashes[file_hash]
+                        log.write(f"Дубликат найден: {file_path}, удален: {duplicate_path}\n")
+                        try:
+                            os.unlink(file_path)
+                        except Exception as e:
+                            log.write(f"Ошибка при удалении файла {file_path}: {e}\n")
+                    else:
+                        file_hashes[file_hash] = file_path
+
+    return print("Поиск завершен. Дубликаты удалены, информация записана в файл.")
