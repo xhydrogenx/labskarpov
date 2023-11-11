f8bf86a55f60a99ee2dfaea5ba6a84c709f2d0f1
hydrogen <ivan.karpov.88@yandex.ru>
2023-11-11 14:25:14 +0700

    Добавлена сортировка по дню/месяцу/году создания, запись в лог файл. Также добавлены комментарии для большинства функций

--- lab9/app.py
+++ lab9/app.py
@@ -1,10 +1,10 @@
+import asyncio
 import os
 import sys
-import asyncio
 
 import aiofiles
-from PyQt6.QtCore import QEventLoop
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QFileDialog
+
 from module_1 import find_and_remove_duplicates
 from module_2 import sort_photos_by_date
 
@@ -22,8 +22,12 @@ class PhotoOrganizerApp(QMainWindow):
 
         self.action_label = QLabel("Введите путь до папки")
         self.directory_path_input = QLineEdit()
+        self.time_step_input = QLineEdit()
+        self.directory_path_input.setPlaceholderText("Укажите путь")
+        self.time_step_input.setPlaceholderText("Введите шаг сортировки (день, неделя, месяц")
         layout.addWidget(self.action_label)
         layout.addWidget(self.directory_path_input)
+        layout.addWidget(self.time_step_input)
 
         # self.input_label = Q
 
@@ -39,7 +43,7 @@ class PhotoOrganizerApp(QMainWindow):
         self.setGeometry(100, 100, 400, 200)
 
         self.find_duplicates_button.clicked.connect(self.start_find_and_remove_duplicates)
-        self.sort_photos_button.clicked.connect(self.sort_photos_by_date)
+        self.sort_photos_button.clicked.connect(self.start_sort_photos_by_date)
         self.open_log_button.clicked.connect(self.open_log)
 
     async def find_and_remove_duplicates(self, directory_to_scan, log_file_path):
@@ -48,11 +52,15 @@ class PhotoOrganizerApp(QMainWindow):
 
     def start_find_and_remove_duplicates(self):
         directory_to_scan = self.directory_path_input.text()  # Получить путь из текстового поля
+
         if directory_to_scan:
-            log_directory = os.path.join(directory_to_scan, "logs")
-            os.makedirs(log_directory, exist_ok=True)
-            log_file_path = os.path.join(log_directory, "duplicate_log.txt")
+            log_directory = os.path.join(directory_to_scan, "logs")  # Путь к директории для журнала
+            os.makedirs(log_directory, exist_ok=True)  # Создание директории для журнала, если она не существует
+            log_file_path = os.path.join(log_directory, "duplicate_log.txt")  # Формирование пути к файлу журнала
+
+            # Запуск асинхронной функции find_and_remove_duplicates
             result = asyncio.run(self.find_and_remove_duplicates(directory_to_scan, log_file_path))
+
             if result:
                 self.action_label.setText("Дубликаты найдены и удалены")
             else:
@@ -60,19 +68,27 @@ class PhotoOrganizerApp(QMainWindow):
         else:
             self.action_label.setText("Укажите путь к папке")
 
-    def sort_photos_by_date(self):
-        directory_to_sort = "C:/Users/hydrogen/PycharmProjects/labskarpov/test"
-        sort_photos_by_date(directory_to_sort)
+    async def sort_photos_by_date(self, directory_to_sort, time_step, log_file_path):
+        return sort_photos_by_date(directory_to_sort, time_step, log_file_path)
+
+    def start_sort_photos_by_date(self):
+        directory_to_sort = self.directory_path_input.text()
+        time_step = self.time_step_input.text()
+        log_directory = os.path.join(directory_to_sort, "logs")
+        os.makedirs(log_directory, exist_ok=True)
+        log_file_path = os.path.join(log_directory, "sort_log.txt")
+        asyncio.run(self.sort_photos_by_date(directory_to_sort, time_step, log_file_path))
 
     def open_log(self):
-        loop = asyncio.ProactorEventLoop()
-        asyncio.set_event_loop(loop)
+        loop = asyncio.ProactorEventLoop()  # Задается цикл событий
+        asyncio.set_event_loop(loop)  # Установка в качестве текущего
         loop.run_until_complete(self.open_log_async())
 
     async def open_log_async(self):
-        directory = self.directory_path_input.text()  # Получить путь из текстового поля
+        directory = self.directory_path_input.text()
         dialog = QFileDialog()
         options = dialog.options()
+        # Можно выбрать только txt файлы, _ здесь и в других местах - флаг успешного/отмененного действия
         fileName, _ = dialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", directory,
                                              "Text Files (*.txt);;All Files (*)", options=options)
         if fileName:
--- lab9/module_1.py
+++ lab9/module_1.py
@@ -1,31 +1,32 @@
-import os
-import hashlib
-import asyncio
 import datetime
+import hashlib
+import os
 
 
 async def find_and_remove_duplicates(directory: str, log_file: str):
     """Поиск файлов происходит по хэшу"""
-    file_hashes = {}
+    file_hashes = {}  # Словарь для хранения хешей
 
     with open(log_file, "a", encoding="utf-8") as log:
         # log.truncate(0)
         for root, _, files in os.walk(directory):
             for file in files:
-                file_path = os.path.join(root, file)
+                file_path = os.path.join(root, file)  # Формирование полного пути к файлу
                 with open(file_path, "rb") as f:
-                    file_content = f.read()
+                    file_content = f.read()  # Чтение содержимого файла в бинарном режиме (по сути fopen(
+                    # "filename.b", "rb"))
                     f.close()
-                    file_hash = hashlib.md5(file_content).hexdigest()
+                    file_hash = hashlib.md5(file_content).hexdigest()  # Подсчет хэша файла
                     if file_hash in file_hashes:
-                        duplicate_path = file_hashes[file_hash]
+                        duplicate_path = file_hashes[file_hash]  # Если хэш уже есть в словаре - дубликат
                         current_time = datetime.datetime.now()
+                        # Запись в лог файл
                         log.write(f"{current_time} | Дубликат найден: {file_path}, удален: {duplicate_path}\n")
                         try:
-                            os.unlink(file_path)
+                            os.unlink(file_path)  # Удаление дубликата
                         except Exception as e:
                             log.write(f"Ошибка при удалении файла {file_path}: {e}\n")
                     else:
-                        file_hashes[file_hash] = file_path
+                        file_hashes[file_hash] = file_path  # Запись хэша файла в словаре (для последующего сравнения)
 
     return print("Поиск завершен. Дубликаты удалены, информация записана в файл.")
